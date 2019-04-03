8d2bc0e1498cda7506dd4a131438886579d9bc4a
Sudip Adhikari <sudeep_sudeep07@hotmail.com>
2019-04-02 19:36:21 -0700

    day2, constructor and tables

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 We have created a website for our friend Pat and his new business Pat's Salmon Cookies. 
+
+On part 1 of this project, we did the following:{
 This webpage utilizes JS, CSS and HTML. 
 Pat wanted to get a website that would give him total cookies per hours in each location.
 There are five lists to show total cookies consumption each hour in different locations.
@@ -11,3 +13,11 @@ Thanks to Dana and Sam for helping me with coding.
 
 Following is coding reference:
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+}
+
+On the second part of the project, we made following changes:
+We used constructor functions which helped us reduce the number of times we were repeating the codes.
+We started using loops to help us minimize the lines of codes we were writing. 
+
+I referenced Sam's codes from classroom activity ery much to help me get this project right. 
+Dana had to guide me through each steps and much thanks to her, this project is up and running.
--- js/app.js
+++ js/app.js
@@ -1,163 +1,100 @@
 'use strict';
 
-var operatingHours=['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm', 'total'];
+var operatingHours=['6 am', '7 am', '8 am', '9 am', '10 am', '11 am', '12 pm', '1 pm', '2 pm', '3 pm', '4 pm', '5 pm', '6 pm', '7 pm', '8 pm'];
 
-var location1={
-  min:23,
-  max:65,
-  avgPerCust:6.3,
-  startingNumbers: [this.min, this.max, this.avgPerCust],
-  cookiesSalesRecord:[],
-  totalSales: 0,
-  cookiesSales: function() {
-    for (var i=0; i<operatingHours.length;i++){
-      //Source:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-      //with assistance from Dana
-      var outputRandom=  Math.floor(Math.random() * (this.max - this.min)) + this.min;
-      this.cookiesSalesRecord[i] = Math.floor(this.avgPerCust*outputRandom);
-      this.totalSales=this.totalSales+this.cookiesSalesRecord[i];
-      console.log(this.totalSales);
-    }this.cookiesSalesRecord.push(this.totalSales);
-    console.log(this.cookiesSalesRecord);
-  },
+var locationDetailsArray=[];
+
+function LocationDetails(locationName, minCustomersHourly, avgPerCustHourly, maxCustomersHourly, cookiesSalesHourly, randomCustomersHourly, randomCookiesHourly, totalSalesDaily){
+  this.locationName=locationName;
+  this.minCustomersHourly=minCustomersHourly;
+  this.maxCustomersHourly=maxCustomersHourly;
+  this.avgPerCustHourly=avgPerCustHourly;
+  this.cookiesSalesHourly=cookiesSalesHourly;
+  this.randomCustomersHourly=randomCustomersHourly;
+  this.randomCookiesHourly=randomCookiesHourly;
+  this.totalSalesDaily=totalSalesDaily;
+}
+
+//With help from Saurav Kadaria
+var location1= new LocationDetails('First and Pike', 23, 6.3, 65, [], [], [], 0);
+locationDetailsArray.push(location1);
+var location2= new LocationDetails('SeaTac Airport', 3, 1.2, 24, [], [], [], 0);
+locationDetailsArray.push(location2);
+var location3= new LocationDetails('Seattle Center',	11,	3.7, 38, [], [], [], 0);
+locationDetailsArray.push(location3);
+var location4= new LocationDetails('Capitol Hill',	20,	2.3, 38,	 [], [], [], 0);
+locationDetailsArray.push(location4);
+var location5= new LocationDetails('Alki',	2, 4.6,	16, [], [], [], 0);
+locationDetailsArray.push(location5);
+
+//Following codes have been referenced from Sam's class lectures
+LocationDetails.prototype.calculateRandomCustomersHourly= function(){
+  for (var i=0; i<operatingHours.length; i++){
+    this.randomCustomersHourly.push(randomNumberGenerator(this.minCustomersHourly, this.maxCustomersHourly));
+  }
 };
-location1.cookiesSales();
-//With assistance from Sam
-//A variable to hold all <ul> elements
-var ulEl= document.getElementById('salesList1');
-for (var i=0; i<operatingHours.length;i++){
-  //create a <li> element
-  var liEl= document.createElement('li');
-  //give it content
-  liEl.textContent=operatingHours[i] + ' : ' + location1.cookiesSalesRecord[i+1];
-  //put it in the DOM
-  ulEl.appendChild(liEl);
+
+LocationDetails.prototype.calculateRandomCookiesHourly= function(){
+  for (var i=0; i<operatingHours.length; i++){
+    var hourlySales= Math.ceil(this.randomCustomersHourly[i]*this.avgPerCustHourly);
+    this.cookiesSalesHourly.push(hourlySales);
+    this.totalSalesDaily += hourlySales;
+  }
+};
+
+function randomNumberGenerator(min, max){
+  return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+//With assistance from Dana
+function timeHeader(){
+  var trEl = document.createElement('tr');
+  var thLocation= document.createElement('th');
+  thLocation.textContent='Location';
+  trEl.appendChild(thLocation);
+  for (var i=0; i<operatingHours.length; i++){
+    var thEl=document.createElement('th');
+    thEl.textContent = operatingHours[i];
+    trEl.appendChild(thEl);
+  }
+  var totalEachLocation=document.createElement('th');
+  totalEachLocation.textContent='Total';
+  trEl.appendChild(totalEachLocation);
+  report.appendChild(trEl);
 }
 
 
+function fillWithCookies(){
+  for (var i=0;i<5;i++){
+    var trEl= document.createElement('tr');
+    var thLocation= document.createElement('th');
+    thLocation.textContent=locationDetailsArray[i].locationName;
+    trEl.appendChild(thLocation);
+    for (var j=0;j<operatingHours.length;j++)
+    {
+      var tdEl= document.createElement('td');
+      tdEl.textContent=locationDetailsArray[i].cookiesSalesHourly[j];
+      trEl.appendChild(tdEl);
+    }
+    var totalCookies=document.createElement('td');
+    totalCookies.textContent=locationDetailsArray[i].totalSalesDaily;
+    trEl.appendChild(totalCookies);
+    report.appendChild(trEl);
+  }
 
-var location2={
-  min:3,
-  max:24,
-  avgPerCust:1.2,
-  startingNumbers: [this.min, this.max, this.avgPerCust],
-  cookiesSalesRecord:[],
-  totalSales: 0,
-  cookiesSales: function() {
-    for (var j=0; j<operatingHours.length;j++){
-      //Source:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-      //with assistance from Dana
-      var outputRandom=  Math.floor(Math.random() * (this.max - this.min)) + this.min;
-      this.cookiesSalesRecord[j] = Math.floor(this.avgPerCust*outputRandom);
-      this.totalSales+= this.cookiesSalesRecord[j];
-    }this.cookiesSalesRecord.push(this.totalSales);
-  },
-};
-location2.cookiesSales();
-//With assistance from Sam
-//A variable to hold all <ul> elements
-var ulEl2= document.getElementById('salesList2');
-for (var j=0; j<operatingHours.length;j++){
-  //create a <li> element
-  var liEl2= document.createElement('li');
-  //give it content
-  liEl2.textContent=operatingHours[j] + ' : ' + location2.cookiesSalesRecord[j+1];
-  //put it in the DOM
-  ulEl2.appendChild(liEl2);
 }
 
+var report= document.getElementById('displayLocations');
 
+LocationDetails.prototype.render=function(){
 
-
-var location3={
-  min:11,
-  max:38,
-  avgPerCust:3.7,
-  startingNumbers: [this.min, this.max, this.avgPerCust],
-  cookiesSalesRecord:[],
-  totalSales: 0,
-  cookiesSales: function() {
-    for (var k=0; k<operatingHours.length;k++){
-      //Source:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-      //with assistance from Dana
-      var outputRandom=  Math.floor(Math.random() * (this.max - this.min)) + this.min;
-      this.cookiesSalesRecord[k] = Math.floor(this.avgPerCust*outputRandom);
-      this.totalSales+= this.cookiesSalesRecord[k];
-    }this.cookiesSalesRecord.push(this.totalSales);
-  },
 };
-location3.cookiesSales();
-//With assistance from Sam
-//A variable to hold all <ul> elements
-var ulEl3= document.getElementById('salesList3');
-for (var k=0; k<operatingHours.length;k++){
-  //create a <li> element
-  var liEl3= document.createElement('li');
-  //give it content
-  liEl3.textContent=operatingHours[k] + ' : ' + location3.cookiesSalesRecord[k+1];
-  //put it in the DOM
-  ulEl3.appendChild(liEl3);
+
+for (var i=0; i<5; i++){
+  locationDetailsArray[i].calculateRandomCustomersHourly();
+  locationDetailsArray[i].calculateRandomCookiesHourly();
+  locationDetailsArray[i].render('salesList'+(i+1));
 }
+timeHeader();
+fillWithCookies();
 
-
-
-var location4={
-  min:20,
-  max:38,
-  avgPerCust:2.3,
-  startingNumbers: [this.min, this.max, this.avgPerCust],
-  cookiesSalesRecord:[],
-  totalSales: 0,
-  cookiesSales: function() {
-    for (var m=0; m<operatingHours.length;m++){
-      //Source:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-      //with assistance from Dana
-      var outputRandom=  Math.floor(Math.random() * (this.max - this.min)) + this.min;
-      this.cookiesSalesRecord[m] = Math.floor(this.avgPerCust*outputRandom);
-      this.totalSales+= this.cookiesSalesRecord[m];
-    }this.cookiesSalesRecord.push(this.totalSales);
-  },
-};
-location4.cookiesSales();
-//With assistance from Sam
-//A variable to hold all <ul> elements
-var ulEl4= document.getElementById('salesList4');
-for (var m=0; m<operatingHours.length;m++){
-  //create a <li> element
-  var liEl4= document.createElement('li');
-  //give it content
-  liEl4.textContent=operatingHours[m] + ' : ' + location4.cookiesSalesRecord[m+1];
-  //put it in the DOM
-  ulEl4.appendChild(liEl4);
-}
-
-
-var location5={
-  min:2,
-  max:operatingHours.length,
-  avgPerCust:4.6,
-  startingNumbers: [this.min, this.max, this.avgPerCust],
-  cookiesSalesRecord:[],
-  totalSales: 0,
-  cookiesSales: function() {
-    for (var n=0; n<operatingHours.length;n++){
-      //Source:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-      //with assistance from Dana
-      var outputRandom=  Math.floor(Math.random() * (this.max - this.min)) + this.min;
-      this.cookiesSalesRecord[n] = Math.floor(this.avgPerCust*outputRandom);
-      this.totalSales+= this.cookiesSalesRecord[n];
-    }this.cookiesSalesRecord.push(this.totalSales);
-  },
-};
-location5.cookiesSales();
-//With assistance from Sam
-//A variable to hold all <ul> elements
-var ulEl5= document.getElementById('salesList5');
-for (var n=0; n<operatingHours.length;n++){
-  //create a <li> element
-  var liEl5= document.createElement('li');
-  //give it content
-  liEl5.textContent=operatingHours[n] + ' : ' + location5.cookiesSalesRecord[n+1];
-  //put it in the DOM
-  ulEl5.appendChild(liEl5);
-}
